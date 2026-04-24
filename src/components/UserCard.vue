cb07385b9e7890c4973c950d8341cbdbeeb4219b
rosarioconde <conde.rosario88@gmail.com>
2026-04-23 21:56:40 -0300

    add defineProps component child

---
+++ src/components/UserCard.vue
@@ -0,0 +1,12 @@
+<template>  
+  <div class="user-card p-4 bg-gray-100 rounded">
+    <h3>{{ user.name }}</h3>
+    <p>{{ user.email }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  user: Object
+})
+</script>
